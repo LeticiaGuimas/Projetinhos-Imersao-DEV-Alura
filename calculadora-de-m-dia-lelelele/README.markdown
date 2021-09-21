221b07cd740b8b2acc1c2c731ba15bb08f44d6a2
LeticiaGuimas <89053821+LeticiaGuimas@users.noreply.github.com>
2021-09-21 11:19:47 -0300

    Add files via upload

---
+++ calculadora-de-m-dia-lelelele/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média Lelelele
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/leticiaguimas/pen/VwWrQxg](https://codepen.io/leticiaguimas/pen/VwWrQxg).
+
+Criando uma calculadora de médias de notas com a Alura
---
+++ calculadora-de-m-dia-lelelele/src/script.js
@@ -0,0 +1,10 @@
+var NotaDoPrimeiroBimestre = 10
+var NotaDoSegundoBimestre = 9
+var NotaDoTerceiroBimestre = 8
+var NotaDoQuartoBimestre = 7
+
+var NotaFinal = (NotaDoPrimeiroBimestre + NotaDoSegundoBimestre + NotaDoTerceiroBimestre + NotaDoQuartoBimestre) / 4
+
+var NotaFixada = NotaFinal.toFixed(0);
+
+console.log(NotaFixada);
